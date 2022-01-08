f6bf53a639399117ee0c8eac773b2ab6b443a2fd
liujiayii <liujiayii@foxmail.com>
2022-01-09 02:41:04 +0800

    feat: init

--- config/config.ts
+++ config/config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   title: 'Element Plus React',
   mode: 'site',
   hash: true,
+  base: process.env.NODE_ENV === 'production' ? '/element-plus-react/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/element-plus-react/' : '/',
+  outputPath: 'dist',
   resolve:{
     includes: ['docs','components']
   },
