e0d7a855cdd62d52b1d6fd4bf7da69c5f2ca8b37
liujiayii <liujiayii@foxmail.com>
2022-01-31 14:51:55 +0800

    fix: Card组件支持style

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -7,13 +7,14 @@ interface CardProps {
   shadow?: 'always' | 'hover'|'never'
   bodyStyle?: React.CSSProperties
   className?: string
+  style?: React.CSSProperties
 }
 
 const Card: React.FC<CardProps> = (props) => {
   const classNames = classnames('el-card',props.className, props?.shadow ? `is-${  props.shadow  }-shadow` : 'is-always-shadow')
   const headerNode = props.header ? <div className="el-card__header">{props.header}</div> : null
   return (
-    <div className={classNames}>
+    <div className={classNames} style={props.style}>
       {headerNode}
       <div className="el-card__body" style={props.bodyStyle}>
         {props.children}
