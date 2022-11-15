911e8877bc38379be3dd5d3edf859c5f0d3a0606
liujiayii <liujiayii@foxmail.com>
2022-11-16 00:20:17 +0800

    feat: up deps

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,10 +1,7 @@
-import { GlobalConfig } from '@element-plus/components/config-provider'
-import { easeInOutCubic } from './src/animate'
+import { GlobalConfig } from '@element-plus/components/src/config-provider';
+import { easeInOutCubic } from './src/animate';
 
-export {default as getCssVar} from './src/getCssVar'
-export {default as css2object} from './src/css2object'
+export { default as getCssVar } from './src/getCssVar';
+export { default as css2object } from './src/css2object';
 
-export {
-  GlobalConfig,
-  easeInOutCubic,
-}
+export { GlobalConfig, easeInOutCubic };
