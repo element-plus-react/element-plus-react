ca3c79900e176597f241d472cf7da3b0e6ec0a04
liujiayii <liujiayii@users.noreply.github.com>
2022-12-23 09:34:48 +0000

    deploy: a91ae3616ceead414ae44cb50a41e8ba86634d1c

--- docs__guide__changelog.md.41305958.async.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[9083],{18950:function(l,m,e){e.r(m);var t=e(71706),u=e(53848),o=e(67049),a=e(80831),i=e(34825),h=e(39201),r=e(52355),c=e(79685),_=e(63342);function s(){var d=(0,r.eL)(),n=d.texts;return(0,_.jsx)(r.dY,{children:(0,_.jsxs)(_.Fragment,{children:[(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsx)("style",{scoped:!0,lang:"scss",dangerouslySetInnerHTML:{__html:`
-@at-root .hero-content {
-  padding: 32px;
-
-}
-`}}),(0,_.jsxs)("h1",{id:"\u66F4\u65B0\u65E5\u5FD7",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u66F4\u65B0\u65E5\u5FD7",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"\u66F4\u65B0\u65E5\u5FD7"]}),(0,_.jsxs)("p",{children:[n[0].value,(0,_.jsx)("strong",{children:n[1].value}),n[2].value,(0,_.jsx)("strong",{children:n[3].value}),n[4].value]}),(0,_.jsxs)("p",{children:[n[5].value,(0,_.jsx)("a",{href:"https://github.com/element-plus/element-plus/blob/dev/CHANGELOG.en-US.md",children:n[6].value}),n[7].value]})]}),(0,_.jsx)(Changelog,{})]})})}m.default=s}}]);
