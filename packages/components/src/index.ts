41b10ce1308643a78ad2e6f6e9618198da49aa18
liujiayii <liujiayii@foxmail.com>
2023-01-09 01:53:43 +0800

    feat: father config

---
+++ packages/components/src/index.ts
@@ -0,0 +1,49 @@
+import Affix from './affix';
+import Button from './button';
+import Avatar from './avatar';
+import Breadcrumb from './breadcrumb';
+import Badge from './badge';
+import BackTop from './backtop';
+import Card from './card';
+import Col from './layout/col';
+import ConfigProvider from './config-provider';
+import Container from './container';
+import Divider from './divider';
+import Descriptions from './descriptions';
+import Empty from './empty';
+import Link from './link';
+import PageHeader from './page-header';
+import Row from './layout/row';
+import Result from './result';
+import Space from './space';
+import Steps from './steps';
+import Skeleton from './skeleton';
+import Transition from './transition';
+import Timeline from './timeline';
+import Alert from './alert';
+
+export {
+  Alert,
+  Affix,
+  Avatar,
+  Badge,
+  Breadcrumb,
+  Button,
+  BackTop,
+  Card,
+  ConfigProvider,
+  Col,
+  Container,
+  Divider,
+  Descriptions,
+  Empty,
+  Link,
+  PageHeader,
+  Row,
+  Result,
+  Space,
+  Steps,
+  Transition,
+  Timeline,
+  Skeleton,
+};
---
+++ packages/hooks/src/index.ts
@@ -0,0 +1,4 @@
+import useGlobalConfig from './use-global-config';
+import useNamespace from './use-namespace';
+
+export { useNamespace, useGlobalConfig };
