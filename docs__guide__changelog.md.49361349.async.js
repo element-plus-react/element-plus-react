3ed2298b69ec2735547e23f48e70e921d9ed7f81
liujiayii <liujiayii@users.noreply.github.com>
2023-01-10 14:51:34 +0000

    deploy: fd936ba58b26bb3fa21a4759391ae39c25b28485

---
+++ docs__guide__changelog.md.49361349.async.js
@@ -0,0 +1,6 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[9083],{56552:function(u,s,e){e.r(s);var m=e(15276),o=e(71257),i=e(35671),r=e(21372),a=e(90050),h=e(28498),d=e(37068),x=e(79685),_=e(63342);function l(){var t=(0,d.eL)(),n=t.texts;return(0,_.jsx)(d.dY,{children:(0,_.jsxs)(_.Fragment,{children:[(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsx)("style",{scoped:!0,lang:"scss",dangerouslySetInnerHTML:{__html:`
+@at-root .hero-content {
+  padding: 32px;
+
+}
+`}}),(0,_.jsxs)("h1",{id:"\u66F4\u65B0\u65E5\u5FD7",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u66F4\u65B0\u65E5\u5FD7",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"\u66F4\u65B0\u65E5\u5FD7"]}),(0,_.jsxs)("p",{children:[n[0].value,(0,_.jsx)("strong",{children:n[1].value}),n[2].value,(0,_.jsx)("strong",{children:n[3].value}),n[4].value]}),(0,_.jsxs)("p",{children:[n[5].value,(0,_.jsx)("a",{href:"https://github.com/element-plus/element-plus/blob/dev/CHANGELOG.en-US.md",children:n[6].value}),n[7].value]})]}),(0,_.jsx)(Changelog,{})]})})}s.default=l}}]);
--- docs__guide__changelog.md.4cfbd77c.async.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[9083],{41173:function(u,s,e){e.r(s);var m=e(56353),o=e(16700),i=e(20697),r=e(48490),a=e(5306),h=e(70270),d=e(60666),x=e(79685),_=e(63342);function l(){var t=(0,d.eL)(),n=t.texts;return(0,_.jsx)(d.dY,{children:(0,_.jsxs)(_.Fragment,{children:[(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsx)("style",{scoped:!0,lang:"scss",dangerouslySetInnerHTML:{__html:`
-@at-root .hero-content {
-  padding: 32px;
-
-}
-`}}),(0,_.jsxs)("h1",{id:"\u66F4\u65B0\u65E5\u5FD7",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u66F4\u65B0\u65E5\u5FD7",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"\u66F4\u65B0\u65E5\u5FD7"]}),(0,_.jsxs)("p",{children:[n[0].value,(0,_.jsx)("strong",{children:n[1].value}),n[2].value,(0,_.jsx)("strong",{children:n[3].value}),n[4].value]}),(0,_.jsxs)("p",{children:[n[5].value,(0,_.jsx)("a",{href:"https://github.com/element-plus/element-plus/blob/dev/CHANGELOG.en-US.md",children:n[6].value}),n[7].value]})]}),(0,_.jsx)(Changelog,{})]})})}s.default=l}}]);
