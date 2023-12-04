e67022561cfec5c30cb99147bc439e9fe8c33117
liujiayii <liujiayii@foxmail.com>
2023-12-04 22:30:08 +0800

    feat: update config

---
+++ internal/build/src/index.ts
@@ -0,0 +1,51 @@
+//export * from "@element-plus/components";
+
+import Affix from "@element-plus/components/src/affix";
+import Alert from "@element-plus/components/src/alert";
+import Avatar from "@element-plus/components/src/avatar";
+import BackTop from "@element-plus/components/src/backtop";
+import Badge from "@element-plus/components/src/badge";
+import Breadcrumb from "@element-plus/components/src/breadcrumb";
+import Button from "@element-plus/components/src/button";
+import Card from "@element-plus/components/src/card";
+import ConfigProvider from "@element-plus/components/src/config-provider";
+import Container from "@element-plus/components/src/container";
+import Descriptions from "@element-plus/components/src/descriptions";
+import Divider from "@element-plus/components/src/divider";
+import Empty from "@element-plus/components/src/empty";
+import Col from "@element-plus/components/src/layout/col";
+import Row from "@element-plus/components/src/layout/row";
+import Link from "@element-plus/components/src/link";
+import PageHeader from "@element-plus/components/src/page-header";
+import Result from "@element-plus/components/src/result";
+import Skeleton from "@element-plus/components/src/skeleton";
+import Space from "@element-plus/components/src/space";
+import Steps from "@element-plus/components/src/steps";
+import Timeline from "@element-plus/components/src/timeline";
+import Transition from "@element-plus/components/src/transition";
+
+export {
+  Affix,
+  Alert,
+  Avatar,
+  BackTop,
+  Badge,
+  Breadcrumb,
+  Button,
+  Card,
+  Col,
+  ConfigProvider,
+  Container,
+  Descriptions,
+  Divider,
+  Empty,
+  Link,
+  PageHeader,
+  Result,
+  Row,
+  Skeleton,
+  Space,
+  Steps,
+  Timeline,
+  Transition,
+};
--- packages/element-plus-react/src/index.ts
+++ packages/element-plus-react/src/index.ts
@@ -1,1 +1,1 @@
-export * from '@element-plus/components';
+export * from "@element-plus/components";
