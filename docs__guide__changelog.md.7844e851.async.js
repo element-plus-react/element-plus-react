3571facbab383c706ebc6968a9b20e8ded160165
liujiayii <liujiayii@users.noreply.github.com>
2023-12-05 08:18:13 +0000

    deploy: be767dcdd5fc6080433d9fe7d9185b870c28a3b2

--- docs__guide__changelog.md.7844e851.async.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[9083],{5719:function(u,t,e){e.r(t);var o=e(70281),r=e(37922),a=e(48152),m=e(94978),i=e(83183),c=e(49433),p=e(87566),l=e(77570),h=e(50959),_=e(11527);function s(){var d=(0,l.eL)(),n=d.texts;return(0,_.jsx)(l.dY,{children:(0,_.jsxs)(_.Fragment,{children:[(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsx)("style",{scoped:!0,lang:"scss",dangerouslySetInnerHTML:{__html:`
-@at-root .hero-content {
-  padding: 32px;
-
-}
-`}}),(0,_.jsxs)("h1",{id:"\u66F4\u65B0\u65E5\u5FD7",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u66F4\u65B0\u65E5\u5FD7",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"\u66F4\u65B0\u65E5\u5FD7"]}),(0,_.jsxs)("p",{children:[n[0].value,(0,_.jsx)("strong",{children:n[1].value}),n[2].value,(0,_.jsx)("strong",{children:n[3].value}),n[4].value]}),(0,_.jsxs)("p",{children:[n[5].value,(0,_.jsx)("a",{href:"https://github.com/element-plus/element-plus/blob/dev/CHANGELOG.en-US.md",children:n[6].value}),n[7].value]})]}),(0,_.jsx)(Changelog,{})]})})}t.default=s}}]);
---
+++ docs__index.md.9771e58e.async.js
@@ -0,0 +1,11 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[6935],{64171:function(t,n,_){_.r(n);var m=_(33576),d=_(1133),r=_(79677),o=_(41842),a=_(5909),i=_(47806),c=_(89937),s=_(99035),p=_(50959),e=_(11527);function u(){var l=(0,s.eL)(),h=l.texts;return(0,e.jsx)(s.dY,{children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsx)("style",{scoped:!0,lang:"scss",dangerouslySetInnerHTML:{__html:`
+  .image-wrapper {
+    padding: 15px;
+    background-color: rgb(249, 250, 252);
+    width: 100%;
+    margin-bottom: 15px;
+  }
+  .img {
+      width: 100%;
+    }
+`}}),(0,e.jsx)("div",{className:"image-wrapper",children:(0,e.jsx)("img",{className:"img",alt:"\u4E00\u7EA7\u7C7B\u76EE",src:_(63740)})})]})})})}n.default=u},63740:function(t,n,_){t.exports=_.p+"static/theme-index-blue.c6274367.png"}}]);
