6b359443a728487320b18da60fa35e48bdef1893
liujiayii <liujiayii@users.noreply.github.com>
2024-03-14 06:51:59 +0000

    deploy: ca8eca789fc01d6c5807e04ec499270b9e36c4f8

--- docs__guide__changelog.md.251dad07.async.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[9083],{51348:function(d,s,e){e.r(s);var u=e(28291),o=e(86419),a=e(59258),m=e(73009),i=e(61033),p=e(2940),c=e(84791),n=e(11920),h=e(50959),_=e(11527);function r(){var l=(0,n.eL)(),t=l.texts;return(0,_.jsx)(n.dY,{children:(0,_.jsxs)(_.Fragment,{children:[(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsx)("style",{scoped:!0,lang:"scss",dangerouslySetInnerHTML:{__html:`
-@at-root .hero-content {
-  padding: 32px;
-
-}
-`}}),(0,_.jsxs)("h1",{id:"\u66F4\u65B0\u65E5\u5FD7",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u66F4\u65B0\u65E5\u5FD7",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"\u66F4\u65B0\u65E5\u5FD7"]}),(0,_.jsxs)("p",{children:[t[0].value,(0,_.jsx)("strong",{children:t[1].value}),t[2].value,(0,_.jsx)("strong",{children:t[3].value}),t[4].value]}),(0,_.jsxs)("p",{children:[t[5].value,(0,_.jsx)("a",{href:"https://github.com/element-plus-react/element-plus-react/releases",children:t[6].value}),t[7].value]})]}),(0,_.jsx)(n.Dl,{demo:{id:"docs-guide-changelog-demo-changelog",inline:!0},previewerProps:{}})]})})}s.default=r}}]);
